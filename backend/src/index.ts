63225081b063bc79ef3efd71b42800f22fd9ad4d
Rithk <rithkchaudhary86@gmail.com>
2024-02-25 18:20:17 +0530

    prisma:

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,24 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/api/v1/signup',(c) => {
+  return c.text('This is signup')
+})
+
+app.post('/api/v1/signin',(c) => {
+  return c.text('This is signin')
+})
+
+app.post('/api/v1/blog',(c) => {
+  return c.text('This is blog')
+})
+
+app.put('/api/v1/blog',(c) => {
+  return c.text('This is blog2')
+})
+
+app.get('api/v1/blog/:id',(c)=>{
+  return c.text('This is blog3')
+})
+
 export default app
