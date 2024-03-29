f57b1772cfd1d52fd2dcffae3a3397039f067c9a
Rithk <rithkchaudhary86@gmail.com>
2024-03-29 10:55:30 +0530

    signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
@@ -16,14 +17,19 @@ app.post('/api/v1/signup',async (c) => {
 }).$extends(withAccelerate())
 const body = await c.req.json();
 
-await prisma.user.create({
+const user= await prisma.user.create({
   data: {
   email: body.email,
   password: body.password,
   }
   
 })
-  return c.text('This is signup') 
+const token = await sign({id: user.id}, "secret")
+
+
+  return c.json({
+    jwt: token
+  }) 
 })
 
 app.post('/api/v1/signin',(c) => {
