8d3a87924d66bbf7f7d31e43ad229c2e897fa9ab
Rithk <rithkchaudhary86@gmail.com>
2024-03-31 10:43:56 +0530

    quote Component

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,5 +1,23 @@
-export const Quote=()=>{
-    return <div className="bg-slate-200 h-screen">
-        Hi from quote component
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className=" flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                All Men Have Limits i ignore mine
+                </div>
+                <div className="max-w-md text-xl font-semibold text-left mt-4">
+                Julies Winfield
+            </div>
+            <div className="max-w-md text-sm font-light text-slate-400">
+                CEO| Acme corp
+            </div>
+            
+
+               
+            </div>
+
+        </div>
+        
+
     </div>
 }
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,8 @@
+import { Quote } from "../components/Quote"
+
 export const Signup=()=>{
-    return <div>
-        Signup
+    return <div className="grid grid-cols-2">
+        <div></div>
+        <div className="invisible md:visible"><Quote /></div>
     </div>
 }
